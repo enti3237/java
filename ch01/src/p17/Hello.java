9ca89e49e21f13a14aa2a766b746eaab89eee841
java <java@DESKTOP-D2K14OF>
2019-10-17 11:25:11 +0900

    2019-10-17 ch01 프로젝트 등록

---
+++ ch01/src/p17/Hello.java
@@ -0,0 +1,13 @@
+package p17;
+/* 날짜 : 2019-10-11
+ * 이름 : 이지영
+ * 내용 : 17페이지
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		
+	}
+
+}
